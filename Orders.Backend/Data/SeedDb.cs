78a5b47ff85b85f53f7fba5ddad322c6acb26323
compucodigo <jmph81@gmail.com>
2026-07-21 09:53:01 -0700

    Agregado la siembra en la BD

---
+++ Orders.Backend/Data/SeedDb.cs
@@ -0,0 +1,40 @@
+﻿namespace Orders.Backend.Data;
+
+public class SeedDb
+{
+    private readonly DataContext _context;
+
+    public SeedDb(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SeedAsync()
+    {
+        await _context.Database.EnsureCreatedAsync();
+        await CheckCountriesAsync();
+        await CheckCategoriesAsync();
+    }
+
+    private async Task CheckCategoriesAsync()
+    {
+        if (!_context.Categories.Any())
+        {
+            _context.Categories.Add(new Shared.Entities.Category { Name = "Tecnología" });
+            _context.Categories.Add(new Shared.Entities.Category { Name = "Ropa" });
+            _context.Categories.Add(new Shared.Entities.Category { Name = "Ferreteria" });
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    private async Task CheckCountriesAsync()
+    {
+        if (!_context.Countries.Any())
+        {
+            _context.Countries.Add(new Shared.Entities.Country { Name = "Venezuela" });
+            _context.Countries.Add(new Shared.Entities.Country { Name = "México" });
+            _context.Countries.Add(new Shared.Entities.Country { Name = "Colombia" });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- Orders.Backend/Program.cs
+++ Orders.Backend/Program.cs
@@ -11,13 +11,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalCnn"));
+builder.Services.AddTransient<SeedDb>();
+
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using var scope = scopedFactory!.CreateScope();
+    var service = scope.ServiceProvider.GetService<SeedDb>();
+    service!.SeedAsync().Wait();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
